bf38e938b6254b822b829ab30430a1f0a813e6e6
RekGRpth <rekgrpth@gmail.com>
2018-04-02 07:54:16 +0500

    up

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-#docker build --tag rekgrpth/postgres . && \
-#docker push rekgrpth/postgres && \
+#docker build --tag rekgrpth/postgres . || exit $?
+#docker push rekgrpth/postgres || exit $?
 docker stop postgres
 docker rm postgres
-docker pull rekgrpth/postgres && \
-docker volume create postgres && \
+docker pull rekgrpth/postgres || exit $?
+docker volume create postgres || exit $?
 docker run \
     --add-host `hostname -f`:`ip -4 addr show docker0 | grep -oP 'inet \K[\d.]+'` \
     --detach \
