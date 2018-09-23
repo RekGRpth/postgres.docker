e003e1615cb4da2dfb9c84fd56e32639546a0313
RekGRpth <rekgrpth@gmail.com>
2018-09-23 12:52:40 +0500

    up

--- run.sh
+++ run.sh
@@ -3,7 +3,9 @@
 #docker build --tag rekgrpth/postgres . || exit $?
 #docker push rekgrpth/postgres || exit $?
 docker stop postgres
+docker stop pgqd
 docker rm postgres
+docker rm pgqd
 docker pull rekgrpth/postgres || exit $?
 docker volume create postgres || exit $?
 docker run \
@@ -17,3 +19,14 @@ docker run \
     --restart always \
     --volume postgres:/data \
     rekgrpth/postgres
+docker run \
+    --add-host `hostname -f`:`ip -4 addr show docker0 | grep -oP 'inet \K[\d.]+'` \
+    --detach \
+    --env USER_ID=$(id -u) \
+    --env GROUP_ID=$(id -g) \
+    --link postgres \
+    --name pgqd \
+    --hostname pgqd \
+    --restart always \
+    --volume postgres:/data \
+    rekgrpth/postgres pgqd /data/pgqd/pgqd.ini
