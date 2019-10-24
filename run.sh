21dd0b6ae752f7f65655c5af6ce0cc4d7b3a1bdf
RekGRpth <rekgrpth@gmail.com>
2019-10-24 11:47:23 +0500

    up

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ docker stop postgres
 docker rm postgres
 docker pull rekgrpth/postgres || exit $?
 docker volume create postgres || exit $?
-docker network create my
+docker network create --opt com.docker.network.bridge.name=docker docker
 docker run \
     --detach \
     --env GROUP_ID=$(id -g) \
@@ -14,7 +14,7 @@ docker run \
     --hostname postgres \
     --link nginx:$(hostname -f) \
     --name postgres \
-    --network my \
+    --network docker \
     --publish 5432:5432 \
     --restart always \
     --volume /etc/certs/$(hostname -d).crt:/etc/ssl/server.crt \
