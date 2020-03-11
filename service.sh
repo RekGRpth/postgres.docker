b4054a6ea395e36b48409b2376ea4256ce3f1465
RekGRpth <rekgrpth@gmail.com>
2020-03-11 10:47:34 +0500

    up

--- service.sh
+++ service.sh
@@ -4,20 +4,16 @@
 #docker push rekgrpth/postgres || exit $?
 docker pull rekgrpth/postgres || exit $?
 docker volume create postgres || exit $?
-docker network create --attachable --opt com.docker.network.bridge.name=docker docker || echo $?
-docker stop postgres || echo $?
-docker rm postgres || echo $?
-docker run \
-    --detach \
+docker network create --attachable --driver overlay docker || echo $?
+docker service create \
     --env GROUP_ID=$(id -g) \
     --env LANG=ru_RU.UTF-8 \
     --env TZ=Asia/Yekaterinburg \
     --env USER_ID=$(id -u) \
     --hostname postgres \
+    --mount type=bind,source=/etc/certs,destination=/etc/certs \
+    --mount type=volume,source=postgres,destination=/home \
     --name postgres \
     --network name=docker \
     --publish target=5432,published=5432,mode=host \
-    --restart always \
-    --volume /etc/certs/:/etc/certs \
-    --volume postgres:/home \
     rekgrpth/postgres
