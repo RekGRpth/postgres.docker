56625a52882e6ca77c846b125fdbd0c04b636693
RekGRpth <rekgrpth@gmail.com>
2020-04-10 17:15:24 +0500

    up

--- service.sh
+++ service.sh
@@ -14,10 +14,10 @@ docker service create \
     --constraint node.role==manager \
     --env GROUP_ID=$(id -g) \
     --env LANG=ru_RU.UTF-8 \
-    --env PGMONITOR=true \
+    --env MONITOR="-vvv create monitor --ssl-self-signed --auth trust" \
     --env TZ=Asia/Yekaterinburg \
     --env USER_ID=$(id -u) \
-    --hostname monitor \
+    --hostname tasks.monitor \
     --mount type=bind,source=/etc/certs,destination=/etc/certs,readonly \
     --mount type=volume,source=monitor,destination=/home \
     --name monitor \
@@ -28,8 +28,7 @@ docker service create \
     --constraint node.labels.host==docker-1 \
     --env GROUP_ID=$(id -g) \
     --env LANG=ru_RU.UTF-8 \
-    --env MONITOR=postgres://autoctl_node@monitor:5432/pg_auto_failover \
-    --env PGMONITOR=true \
+    --env MONITOR="-vvv create postgres --ssl-self-signed --auth trust --monitor=postgres://autoctl_node@tasks.monitor:5432/pg_auto_failover" \
     --env TZ=Asia/Yekaterinburg \
     --env USER_ID=$(id -u) \
     --hostname tasks.postgres-1 \
@@ -45,8 +44,7 @@ docker service create \
     --constraint node.labels.host==docker-2 \
     --env GROUP_ID=$(id -g) \
     --env LANG=ru_RU.UTF-8 \
-    --env MONITOR=postgres://autoctl_node@monitor:5432/pg_auto_failover \
-    --env PGMONITOR=true \
+    --env MONITOR="-vvv create postgres --ssl-self-signed --auth trust --monitor=postgres://autoctl_node@tasks.monitor:5432/pg_auto_failover" \
     --env TZ=Asia/Yekaterinburg \
     --env USER_ID=$(id -u) \
     --hostname tasks.postgres-2 \
