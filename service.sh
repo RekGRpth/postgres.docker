a3b5373c274072b9010113f5152b818ad477a8da
RekGRpth <rekgrpth@gmail.com>
2020-04-10 12:12:25 +0500

    up

--- service.sh
+++ service.sh
@@ -3,18 +3,58 @@
 #docker build --tag rekgrpth/postgres . || exit $?
 #docker push rekgrpth/postgres || exit $?
 docker pull rekgrpth/postgres || exit $?
-docker volume create postgres || exit $?
+docker volume create monitor || echo $?
+docker volume create postgres || echo $?
 docker network create --attachable --driver overlay docker || echo $?
+docker service rm postgres-1 || echo $?
+docker service rm postgres-1 || echo $?
+docker service rm monitor || echo $?
+rm /run/postgresql/* || echo $?
 docker service create \
+    --constraint node.role==manager \
     --env GROUP_ID=$(id -g) \
     --env LANG=ru_RU.UTF-8 \
+    --env PGMONITOR=true \
     --env TZ=Asia/Yekaterinburg \
     --env USER_ID=$(id -u) \
-    --hostname postgres \
+    --hostname monitor \
+    --mount type=bind,source=/etc/certs,destination=/etc/certs,readonly \
+    --mount type=volume,source=monitor,destination=/home \
+    --name monitor \
+    --network name=docker \
+    --replicas-max-per-node 1 \
+    rekgrpth/postgres pg_autoctl -vvv run
+docker service create \
+    --constraint node.labels.host==docker-1 \
+    --env GROUP_ID=$(id -g) \
+    --env LANG=ru_RU.UTF-8 \
+    --env MONITOR=postgres://autoctl_node@monitor:5432/pg_auto_failover \
+    --env PGMONITOR=true \
+    --env TZ=Asia/Yekaterinburg \
+    --env USER_ID=$(id -u) \
+    --hostname tasks.postgres-1 \
     --mount type=bind,source=/etc/certs,destination=/etc/certs,readonly \
     --mount type=bind,source=/run/postgresql,destination=/run/postgresql \
     --mount type=volume,source=postgres,destination=/home \
-    --name postgres \
+    --name postgres-1 \
     --network name=docker \
     --publish target=5432,published=5432,mode=host \
-    rekgrpth/postgres
+    --replicas-max-per-node 1 \
+    rekgrpth/postgres pg_autoctl -vvv run
+docker service create \
+    --constraint node.labels.host==docker-2 \
+    --env GROUP_ID=$(id -g) \
+    --env LANG=ru_RU.UTF-8 \
+    --env MONITOR=postgres://autoctl_node@monitor:5432/pg_auto_failover \
+    --env PGMONITOR=true \
+    --env TZ=Asia/Yekaterinburg \
+    --env USER_ID=$(id -u) \
+    --hostname tasks.postgres-2 \
+    --mount type=bind,source=/etc/certs,destination=/etc/certs,readonly \
+    --mount type=bind,source=/run/postgresql,destination=/run/postgresql \
+    --mount type=volume,source=postgres,destination=/home \
+    --name postgres-2 \
+    --network name=docker \
+    --publish target=5432,published=5432,mode=host \
+    --replicas-max-per-node 1 \
+    rekgrpth/postgres pg_autoctl -vvv run
