5161869a06f5824f5f9bc40fdce453db1fbbc0a0
RekGRpth <rekgrpth@gmail.com>
2021-07-30 08:46:48 +0500

    monitor

--- monitor.service.sh
+++ monitor.service.sh
@@ -1,16 +1,13 @@
-#!/bin/sh -ex
+#!/bin/sh -eux
 
-#docker build --tag rekgrpth/postgres .
-#docker push rekgrpth/postgres
-docker pull rekgrpth/postgres
-docker network create --attachable --driver overlay dockers || echo $?
+docker pull ghcr.io/rekgrpth/postgres.docker:REL_13_STABLE
+docker network create --attachable --driver overlay docker || echo $?
 docker volume create monitor
 docker service rm monitor || echo $?
 docker service create \
     --env CLUSTER_NAME=monitor \
     --env GROUP_ID="$(id -g)" \
     --env LANG=ru_RU.UTF-8 \
-    --env PG_AUTOCTL_AUTH=trust \
     --env PG_AUTOCTL_SERVER_CERT=/etc/certs/cert.pem \
     --env PG_AUTOCTL_SERVER_KEY=/etc/certs/key.pem \
     --env PG_AUTOCTL_SSL_CA_FILE=/etc/certs/ca.pem \
@@ -23,6 +20,6 @@ docker service create \
     --mount type=bind,source=/etc/certs,destination=/etc/certs,readonly \
     --mount type=volume,source=monitor,destination=/home \
     --name monitor \
-    --network name=dockers \
+    --network name=docker \
     --replicas-max-per-node 1 \
-    rekgrpth/postgres runsvdir /etc/service
+    ghcr.io/rekgrpth/postgres.docker:REL_13_STABLE runsvdir /etc/service
