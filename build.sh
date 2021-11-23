df240520a28e18963264533b7739925b9ade3a04
RekGRpth <rekgrpth@gmail.com>
2021-11-23 08:34:41 +0500

    alpine-12

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -eux
 
-DOCKER_BUILDKIT=1 docker build --progress=plain --tag ghcr.io/rekgrpth/postgres.docker:alpine-13 . 2>&1 | tee build.log
+DOCKER_BUILDKIT=1 docker build --progress=plain --tag ghcr.io/rekgrpth/postgres.docker:alpine-12 . 2>&1 | tee build.log
