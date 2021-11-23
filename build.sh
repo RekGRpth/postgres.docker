55907e28b42422cc8285e1ad74029018a49c51e4
RekGRpth <rekgrpth@gmail.com>
2021-11-23 08:27:45 +0500

    ubuntu-11

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -eux
 
-DOCKER_BUILDKIT=1 docker build --progress=plain --tag ghcr.io/rekgrpth/postgres.docker:ubuntu-14 . 2>&1 | tee build.log
+DOCKER_BUILDKIT=1 docker build --progress=plain --tag ghcr.io/rekgrpth/postgres.docker:ubuntu-11 . 2>&1 | tee build.log
