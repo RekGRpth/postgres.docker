338735c22ce34177ddbe9cc9dd5402cd68b23cd2
RekGRpth <rekgrpth@gmail.com>
2022-01-09 13:18:43 +0500

    9.4

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -eux
 
-DOCKER_BUILDKIT=1 docker build --progress=plain --tag ghcr.io/rekgrpth/postgres.docker:REL9_5_STABLE . 2>&1 | tee build.log
+DOCKER_BUILDKIT=1 docker build --progress=plain --tag ghcr.io/rekgrpth/postgres.docker:REL9_4_STABLE . 2>&1 | tee build.log
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -eux
 
-docker pull ghcr.io/rekgrpth/postgres.docker:REL9_5_STABLE
+docker pull ghcr.io/rekgrpth/postgres.docker:REL9_4_STABLE
 docker network create --attachable --opt com.docker.network.bridge.name=docker docker || echo $?
 docker volume create postgres
 docker stop postgres || echo $?
@@ -20,4 +20,4 @@ docker run \
     --publish target=5432,published=5432,mode=host \
     --restart always \
     --shm-size=4G \
-    ghcr.io/rekgrpth/postgres.docker:REL9_5_STABLE runsvdir /etc/service
+    ghcr.io/rekgrpth/postgres.docker:REL9_4_STABLE runsvdir /etc/service
