9915d1b000b79dafbd7aba4129034457bedc66f3
RekGRpth <rekgrpth@gmail.com>
2022-01-16 17:29:20 +0500

    fix

--- bin/docker_clean_su-exec.sh
+++ bin/docker_clean_su-exec.sh
@@ -4,7 +4,7 @@ cd /
 apk add --no-cache --virtual .postgresql-rundeps \
     openssh-client \
     su-exec \
-    $(scanelf --needed --nobanner --format '%n#p' --recursive /usr/local | tr ',' '\n' | grep -v -e gdal -e geos -e perl -e python -e tcl | sort -u | while read -r lib; do test ! -e "/usr/local/lib/$lib" && echo "so:$lib"; done) \
+    $(scanelf --needed --nobanner --format '%n#p' --recursive /usr/local | tr ',' '\n' | grep -v -e gdal -e geos -e perl -e proj -e python -e tcl | sort -u | while read -r lib; do test ! -e "/usr/local/lib/$lib" && echo "so:$lib"; done) \
 ;
 find /usr/local/bin -type f -exec strip '{}' \;
 find /usr/local/lib -type f -name "*.so" -exec strip '{}' \;
