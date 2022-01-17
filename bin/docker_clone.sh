c99b96bf5928c83f943846eaf5149794cf313617
RekGRpth <rekgrpth@gmail.com>
2022-01-17 11:03:14 +0500

    fix

--- bin/docker_clone.sh
+++ bin/docker_clone.sh
@@ -36,6 +36,8 @@ if [ "$DOCKER_BUILD" = "build" ]; then
     git clone -b master https://github.com/RekGRpth/pg_repack.git
     git clone -b master https://github.com/RekGRpth/plsh.git
     git clone -b master https://github.com/RekGRpth/postgis.git
-    git clone -b master https://github.com/RekGRpth/repack_bgw.git
+    if [ "$DOCKER_POSTGRES_BRANCH" != "REL_10_STABLE" ] && [ "$DOCKER_POSTGRES_BRANCH" != "REL9_6_STABLE" ] && [ "$DOCKER_POSTGRES_BRANCH" != "REL9_5_STABLE" ] && [ "$DOCKER_POSTGRES_BRANCH" != "REL9_4_STABLE" ]; then
+        git clone -b master https://github.com/RekGRpth/repack_bgw.git
+    fi
     git clone -b master --recursive https://github.com/RekGRpth/pgqd.git
 fi
