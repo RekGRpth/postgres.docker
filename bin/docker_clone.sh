64baa578496b4899369e0b273369ed59792db833
RekGRpth <rekgrpth@gmail.com>
2022-01-17 15:14:49 +0500

    fix

--- bin/docker_clone.sh
+++ bin/docker_clone.sh
@@ -32,7 +32,13 @@ if [ "$DOCKER_POSTGRES_BRANCH" != "REL9_6_STABLE" ] && [ "$DOCKER_POSTGRES_BRANC
 fi
 git clone -b master https://github.com/RekGRpth/powa-archivist.git
 git clone -b master https://github.com/RekGRpth/prefix.git
-git clone -b master https://github.com/RekGRpth/session_variable.git
+if [ "$DOCKER_BUILD" = "build" ]; then
+    git clone -b master https://github.com/RekGRpth/session_variable.git
+else
+    if [ "$DOCKER_POSTGRES_BRANCH" != "REL_10_STABLE" ] && [ "$DOCKER_POSTGRES_BRANCH" != "REL9_6_STABLE" ] && [ "$DOCKER_POSTGRES_BRANCH" != "REL9_5_STABLE" ] && [ "$DOCKER_POSTGRES_BRANCH" != "REL9_4_STABLE" ]; then
+        git clone -b master https://github.com/RekGRpth/session_variable.git
+    fi
+fi
 git clone -b REL1_STABLE https://github.com/RekGRpth/hypopg.git
 if [ "$DOCKER_BUILD" = "build" ]; then
     git clone -b "$DOCKER_POSTGRES_BRANCH" https://github.com/RekGRpth/postgres.git
