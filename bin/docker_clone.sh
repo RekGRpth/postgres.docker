48ac4af40d2bc8e8f52e923e1fcea633b0a55652
RekGRpth <rekgrpth@gmail.com>
2022-01-17 12:44:28 +0500

    fix

--- bin/docker_clone.sh
+++ bin/docker_clone.sh
@@ -19,7 +19,9 @@ git clone -b master https://github.com/RekGRpth/pg_task.git
 git clone -b master https://github.com/RekGRpth/pg_track_settings.git
 git clone -b master https://github.com/RekGRpth/pg_wait_sampling.git
 git clone -b master https://github.com/RekGRpth/pldebugger.git
-git clone -b master https://github.com/RekGRpth/plpgsql_check.git
+if [ "$DOCKER_POSTGRES_BRANCH" != "REL9_6_STABLE" ] && [ "$DOCKER_POSTGRES_BRANCH" != "REL9_5_STABLE" ] && [ "$DOCKER_POSTGRES_BRANCH" != "REL9_4_STABLE" ]; then
+    git clone -b master https://github.com/RekGRpth/plpgsql_check.git
+fi
 git clone -b master https://github.com/RekGRpth/powa-archivist.git
 git clone -b master https://github.com/RekGRpth/prefix.git
 git clone -b master https://github.com/RekGRpth/session_variable.git
