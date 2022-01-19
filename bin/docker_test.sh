e740bda07005f0cb1b0b9b663b928b76ed2f3fb4
RekGRpth <rekgrpth@gmail.com>
2022-01-19 07:54:55 +0500

    fix

--- bin/docker_test.sh
+++ bin/docker_test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -eux
 
 cd /
-gosu postgres pg_ctl initdb
+gosu postgres pg_ctl initdb -k
 echo "max_worker_processes = '128'" >>"$PGDATA/postgresql.auto.conf"
 PG_VERSION_NUM="$(cat /usr/local/include/postgresql/server/pg_config.h | grep PG_VERSION_NUM | cut -f3 -d ' ')"
 if [ "$PG_VERSION_NUM" -ge 90600 ]; then
@@ -9,7 +9,7 @@ if [ "$PG_VERSION_NUM" -ge 90600 ]; then
 else
     echo "shared_preload_libraries = 'auto_explain,pg_stat_statements,pg_stat_kcache,pg_qualstats,plugin_debugger,pg_partman_bgw,pg_task'" >>"$PGDATA/postgresql.auto.conf"
 fi
-gosu postgres pg_ctl start
+gosu postgres pg_ctl -w start
 sleep 10
 export PGUSER=postgres
 find "$HOME/src" -maxdepth 1 -mindepth 1 -type d | grep -v -e src/pg_task -e src/postgres | sort -u | while read -r NAME; do
@@ -19,3 +19,4 @@ done
 cd "$HOME/src/pg_task"
 export PGDATABASE=postgres
 make -j"$(nproc)" USE_PGXS=1 installcheck CONTRIB_TESTDB="$PGDATABASE" || (cat "$HOME/src/pg_task/regression.diffs"; exit 1)
+gosu postgres pg_ctl -m fast -w stop
