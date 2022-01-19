8e77832ae581474d4f3905a615ffaf2ab5991553
RekGRpth <rekgrpth@gmail.com>
2022-01-19 18:34:21 +0500

    fix

--- bin/docker_clean_alpine.sh
+++ bin/docker_clean_alpine.sh
@@ -3,7 +3,7 @@
 cd /
 apk add --no-cache --virtual .postgresql-rundeps \
     openssh-client \
-    $(scanelf --needed --nobanner --format '%n#p' --recursive /usr/local | tr ',' '\n' | grep -v -e gdal -e geos -e perl -e proj -e python -e tcl | sort -u | while read -r lib; do test ! -e "/usr/local/lib/$lib" && echo "so:$lib"; done) \
+    $(scanelf --needed --nobanner --format '%n#p' --recursive /usr/local | tr ',' '\n' | grep -v -e gdal -e geos -e perl -e proj -e python -e tcl | sort -u | while read -r lib; do test -z "$(find /usr/local/lib -name "$lib")" && echo "so:$lib"; done) \
 ;
 find /usr/local/bin -type f -exec strip '{}' \;
 find /usr/local/lib -type f -name "*.so" -exec strip '{}' \;
